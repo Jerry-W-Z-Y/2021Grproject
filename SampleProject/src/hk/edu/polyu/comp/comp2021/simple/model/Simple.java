ccc96f70f68fb1fecfe487e10c63d4e44e45a1ce
chukahang0 <117981013+chukahang0@users.noreply.github.com>
2022-11-13 22:49:20 +0800

    Update Simple.java

--- SampleProject/src/hk/edu/polyu/comp/comp2021/simple/model/Simple.java
+++ SampleProject/src/hk/edu/polyu/comp/comp2021/simple/model/Simple.java
@@ -52,44 +52,141 @@ public class Simple {
         }
         return k;
     }
-    public Simple binexpr(String a1,String a2 ,String a3,String a4, Simple test[]) {
+        public Simple binexpr(String a1,String a2 ,String a3,String a4, Simple test[]) {
         //code here
 
         Simple b = new Simple();
         int i2,i4;
         boolean b2,b4;
+        b.boolexp = false;
 
         b.expName = a1;
-
+        /*
         for(int i = 0 ; i<test.length;i++){
+
             if(test[i]!=null){
-            if ((a2==test[i].varName)&&(test[i].type=="int")){
+                if ((a2.equals(test[i].varName))&&(test[i].type.equals("int"))){
                     i2 = test[i].integer;
                     a2 = String.valueOf(i2);
-                    break;
-            }
-            if ((a2==test[i].varName)&&(test[i].type=="bool")){
+
+                }
+                if ((a4.equals(test[i].varName))&&(test[i].type.equals("bool"))){
                     b2=  test[i].bool;
                     a2 = String.valueOf(b2);
-                    break;
-            }
+
+                }
             }
         }
         for(int i = 0 ; i<test.length;i++){
             if(test[i]!=null){
-            if ((a4==test[i].varName)&&(test[i].type=="int")){
+            if ((a4.equals(test[i].varName))&&(test[i].type.equals("int"))){
                 i4 = test[i].integer;
                 a4 = String.valueOf(i4);
-                break;
+
             }
-            if ((a4==test[i].varName)&&(test[i].type=="bool")){
+                if ((a4.equals(test[i].varName))&&(test[i].type.equals("bool"))){
                 b4=  test[i].bool;
                 a4= String.valueOf(b4);
-                break;
+
             }
             }
         }
+        */
 
+
+
+
+        b.bop = a3;
+        System.out.println(a2+" "+a3+" "+a4);
+        System.out.println(isNumeric(a2));
+        System.out.println(isNumeric(a4));
+        //check a[2], a[4] whether is variables
+        //if expref1 and expref2 are int {
+        if (((isNumeric(a2))) && (isNumeric(a4))) {  //a2 a4 are integers
+            int x = (Integer.parseInt(a2));
+            int y = Integer.parseInt(a4);
+
+            switch (a3) {
+                case "+":
+                    b.intexp = x + y;
+                    break;
+                case "-":
+                    b.intexp = Integer.parseInt(a2) - Integer.parseInt(a4);
+                    break;
+                case "*":
+                    b.intexp = Integer.parseInt(a2) * Integer.parseInt(a4);
+                    break;
+                case "/":
+                    b.intexp = Integer.parseInt(a2) / Integer.parseInt(a4);
+                    break;
+                case ">":
+                    b.boolexp = Integer.parseInt(a2) > Integer.parseInt(a4);
+                    break;
+                case ">=":
+                    b.boolexp = Integer.parseInt(a2) >= Integer.parseInt(a4);
+                    break;
+                case "<":
+                    b.boolexp = Integer.parseInt(a2) < Integer.parseInt(a4);
+                    break;
+                case "<=":
+                    b.boolexp = Integer.parseInt(a2) <= Integer.parseInt(a4);
+                    break;
+                case "==":
+                    b.boolexp = Integer.parseInt(a2) == Integer.parseInt(a4);
+                    break;
+                case "!=":
+                    b.boolexp = Integer.parseInt(a2) != Integer.parseInt(a4);
+                    break;
+            }
+        }
+
+        else{
+            System.out.println(Boolean.parseBoolean(a2)+" "+Boolean.parseBoolean(a4));
+                switch (a3) {
+                    case ("&&"):
+                        b.boolexp = Boolean.parseBoolean(a2) && Boolean.parseBoolean(a4);
+                        break;
+
+                    case "||":
+                        b.boolexp = Boolean.parseBoolean(a2) || Boolean.parseBoolean(a4);
+                        break;
+
+                    case "==":
+                        b.boolexp = Boolean.parseBoolean(a2) == Boolean.parseBoolean(a4);
+                        break;
+
+                    case "!=":
+                        b.boolexp = Boolean.parseBoolean(a2) != Boolean.parseBoolean(a4);
+                        break;
+
+                }
+            }
+
+
+        System.out.println(b.boolexp+" int "+ b.intexp);
+        return b;
+
+    }
+    public void unexpr(String[] a) {
+        //code here
+        Simple c = new Simple();
+        c.expName = a[1];
+        c.uop = a[2];
+        //if expref1 is int{
+        switch(bop){
+            case "#": ;
+            case "~": ;
+        }
+
+        //if expref1 is bool{
+        switch (bop){
+            case "!": ;
+        }
+        c.expref1 = a[3];
+        System.out.println("unexpr variable");
+    }
+
+}
 
 
 
